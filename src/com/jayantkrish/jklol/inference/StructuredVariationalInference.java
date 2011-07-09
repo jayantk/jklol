2ae8eb6375f17eeaddd0020fa93c7b26625f1016
Jayant Krishnamurthy <jayant@jayant-macbookair.(none)>
2011-07-09 16:03:09 -0400

    src/com/jayantkrish/jklol/inference/StructuredVariationalInference.java

---
+++ src/com/jayantkrish/jklol/inference/StructuredVariationalInference.java
@@ -0,0 +1,63 @@
+package com.jayantkrish.jklol.inference;
+
+import java.util.List;
+
+import com.google.common.base.Function;
+import com.jayantkrish.jklol.models.Factor;
+import com.jayantkrish.jklol.models.FactorGraph;
+import com.jayantkrish.jklol.util.Assignment;
+
+/**
+ * StructuredVariationalInference performs variational inference with respect to a
+ * structured graph (which subsumes mean field). 
+ * @author jayant
+ *
+ */
+public class StructuredVariationalInference implements InferenceEngine {
+
+	private Function<FactorGraph, FactorGraph> simpleModelConstructor;
+	private FactorGraph factorGraph;
+	private FactorGraph structuredApproximation;
+	
+	public StructuredVariationalInference(Function<FactorGraph, FactorGraph> simpleModelConstructor) {
+		this.simpleModelConstructor = simpleModelConstructor;
+		factorGraph = null;
+	}
+	
+	@Override
+	public void computeMarginals() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void computeMarginals(Assignment assignment) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void computeMaxMarginals() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void computeMaxMarginals(Assignment assignment) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Factor getMarginal(List<Integer> varNums) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setFactorGraph(FactorGraph f) {
+		this.factorGraph = f;
+		this.structuredApproximation = simpleModelConstructor.apply(f); 
+	}
+
+}

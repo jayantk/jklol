aef529178dd856af159ca4191b8c242a180b924e
Jayant Krishnamurthy <jayantk@gs9670.sp.cs.cmu.edu>
2011-09-21 14:29:00 -0400

    updates to accumulating counts

--- src/com/jayantkrish/jklol/util/PairCountAccumulator.java
+++ src/com/jayantkrish/jklol/util/PairCountAccumulator.java
@@ -12,37 +12,85 @@ import com.google.common.collect.Maps;
  * @author jayantk
  */
 public class PairCountAccumulator<A, B> {
-  
+
   private final Map<A, Map<B, Double>> counts;
-  
+  private final DefaultHashMap<A, Double> conditionalCounts;
+  private double totalCount;
+
+  /**
+   * Creates an accumulator with a count of 0 for every (A, B) pair.
+   */
   public PairCountAccumulator() {
     counts = Maps.newHashMap();
+    conditionalCounts = new DefaultHashMap<A, Double>(0.0);
+    totalCount = 0.0;
   }
-  
+
+  /**
+   * Increments the count of outcome (first, second) by {@code amount}.
+   */
   public void incrementOutcome(A first, B second, double amount) {
     if (!counts.containsKey(first)) {
-      counts.put(first, Maps.<B, Double>newHashMap());
+      counts.put(first, Maps.<B, Double> newHashMap());
     }
     if (!counts.get(first).containsKey(second)) {
       counts.get(first).put(second, 0.0);
     }
     counts.get(first).put(second, amount + counts.get(first).get(second));
+    conditionalCounts.put(first, amount + conditionalCounts.get(first));
+    totalCount += amount;
   }
-  
+
+  /**
+   * Gets the number of times (first, second) has occurred.
+   */
   public double getCount(A first, B second) {
     if (counts.containsKey(first) && counts.get(first).containsKey(second)) {
       return counts.get(first).get(second);
     }
     return 0.0;
   }
-  
+
+  /**
+   * Gets the total number of observed outcomes.
+   */
+  public double getTotalCount() {
+    return totalCount;
+  }
+
+  /**
+   * Gets the joint probability of observing (first, second). The sum of the
+   * return value of this method over all (A, B) pairs is 1.0.
+   */
+  public double getProbability(A first, B second) {
+    return getCount(first, second) / totalCount;
+  }
+
+  /**
+   * Gets the conditional probability of observing {@code second} given
+   * {@code first}. Returns NaN if {@code first} has never been observed.
+   * 
+   * @param first
+   * @param second
+   * @return
+   */
+  public double getConditionalProbability(A first, B second) {
+    return getCount(first, second) / conditionalCounts.get(first);
+  }
+
+  /**
+   * Gets all B values which have been observed with {@code first}.
+   * 
+   * @param first
+   * @return
+   */
   public Set<B> getValues(A first) {
     if (counts.containsKey(first)) {
       return counts.get(first).keySet();
     }
     return Collections.emptySet();
   }
-  
+
   public Set<A> keySet() {
     return counts.keySet();
   }

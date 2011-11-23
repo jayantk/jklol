fa78e41ca00971d10486e3da92b1c18547b2dc20
Jayant Krishnamurthy <jayantk@gs9670.sp.cs.cmu.edu>
2011-11-22 21:53:19 -0500

    code for performance tests

---
+++ src/com/jayantkrish/jklol/testing/PerformanceTestRunner.java
@@ -0,0 +1,11 @@
+package com.jayantkrish.jklol.testing;
+
+/**
+ * Program for running performance tests, which are methods annotated using
+ * {@link PerformanceTest}.
+ * 
+ * @author jayantk
+ */
+public class PerformanceTestRunner {
+
+}

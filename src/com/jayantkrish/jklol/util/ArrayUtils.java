3a7cef1d68eed40308f17179635f962ef6d1f79c
Jayant Krishnamurthy <jayantk@gs9670.sp.cs.cmu.edu>
2012-11-13 23:31:04 -0500

    gwt stuff

---
+++ src/com/jayantkrish/jklol/util/ArrayUtils.java
@@ -0,0 +1,67 @@
+package com.jayantkrish.jklol.util;
+
+public class ArrayUtils {
+
+  /**
+   * Identical to {@code Arrays.copyOf}, but GWT compatible.
+   */
+  public static int[] copyOf(int[] old, int length) {
+    int[] newArray = new int[length];
+    int minLength = Math.min(old.length, length);
+    System.arraycopy(old, 0, newArray, 0, minLength);
+    return newArray;
+  }
+
+  /**
+   * Identical to {@code Arrays.copyOf}, but GWT compatible.
+   */
+  public static double[] copyOf(double[] old, int length) {
+    double[] newArray = new double[length];
+    int minLength = Math.min(old.length, length);
+    System.arraycopy(old, 0, newArray, 0, minLength);
+    return newArray;
+  }
+
+  /**
+   * Identical to {@code Arrays.copyOf}, but GWT compatible.
+   */
+  public static long[] copyOf(long[] old, int length) {
+    long[] newArray = new long[length];
+    int minLength = Math.min(old.length, length);
+    System.arraycopy(old, 0, newArray, 0, minLength);
+    return newArray;
+  }
+
+  /**
+   * Identical to {@code Arrays.copyOfRange}, but GWT compatible.
+   */
+  public static int[] copyOfRange(int[] old, int from, int to) {
+    int length = to - from;
+    int[] newArray = new int[length];
+    int minLength = Math.min(old.length - from, length);
+    System.arraycopy(old, from, newArray, 0, minLength);
+    return newArray;
+  }
+
+  /**
+   * Identical to {@code Arrays.copyOfRange}, but GWT compatible.
+   */
+  public static long[] copyOfRange(long[] old, int from, int to) {
+    int length = to - from;
+    long[] newArray = new long[length];
+    int minLength = Math.min(old.length - from, length);
+    System.arraycopy(old, from, newArray, 0, minLength);
+    return newArray;
+  }
+
+  /**
+   * Identical to {@code Arrays.copyOfRange}, but GWT compatible.
+   */
+  public static double[] copyOfRange(double[] old, int from, int to) {
+    int length = to - from;
+    double[] newArray = new double[length];
+    int minLength = Math.min(old.length - from, length);
+    System.arraycopy(old, from, newArray, 0, minLength);
+    return newArray;
+  }
+}

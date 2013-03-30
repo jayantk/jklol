ebd3870fded2ada11b38f8083e4334bfffb45ed0
Jayant Krishnamurthy <jayantk@thrust.ML.cmu.edu>
2013-03-30 16:43:24 -0400

    misc training stuff

--- scripts/pos/test_crf.sh
+++ scripts/pos/test_crf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MODEL=out15.ser
+MODEL=out17.ser
 TEST_DATA=~/data/ptb_pos/pos_19-21.txt
 
 ./scripts/run.sh com.jayantkrish.jklol.pos.TestPosCrf --model=$MODEL --testFilename=$TEST_DATA $@
--- scripts/pos/train_crf.sh
+++ scripts/pos/train_crf.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
 TRAINING_DATA=~/data/ptb_pos/pos_00-18.txt
-OUTPUT=out15.ser
+OUTPUT=out17.ser
 
-./scripts/run.sh com.jayantkrish.jklol.pos.TrainPosCrf --training=$TRAINING_DATA --output=$OUTPUT --initialStepSize=0.1 --iterations 10 --l2Regularization 0.00001 --maxThreads 16 --batchSize 100 $@
+./scripts/run.sh com.jayantkrish.jklol.pos.TrainPosCrf --training=$TRAINING_DATA --output=$OUTPUT --initialStepSize=0.1 --iterations 10 --l2Regularization 0.00001 --maxThreads 16 --batchSize 100 --maxMargin $@
 
 echo "Printing top parameters:"
 ./scripts/run.sh com.jayantkrish.jklol.cli.PrintParameters --model=$OUTPUT --numFeatures 10

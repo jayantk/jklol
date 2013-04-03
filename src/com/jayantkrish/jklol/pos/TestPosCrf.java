618379d78585f9e7d0de3275777f29cc7a5cef96
Jayant Krishnamurthy <jayantk@gs9670.sp.cs.cmu.edu>
2013-04-03 11:32:34 -0400

    parallel POS evaluation

--- src/com/jayantkrish/jklol/pos/TestPosCrf.java
+++ src/com/jayantkrish/jklol/pos/TestPosCrf.java
@@ -8,6 +8,10 @@ import joptsimple.OptionSpec;
 
 import com.google.common.base.Preconditions;
 import com.jayantkrish.jklol.cli.AbstractCli;
+import com.jayantkrish.jklol.parallel.MapReduceConfiguration;
+import com.jayantkrish.jklol.parallel.MapReduceExecutor;
+import com.jayantkrish.jklol.parallel.Mapper;
+import com.jayantkrish.jklol.parallel.Reducer.SimpleReducer;
 import com.jayantkrish.jklol.util.IoUtils;
 
 public class TestPosCrf extends AbstractCli {
@@ -34,27 +38,12 @@ public class TestPosCrf extends AbstractCli {
       List<PosTaggedSentence> testData = PosTaggerUtils.readTrainingData(
           options.valueOf(testFilename));
 
-      int numCorrect = 0;
-      int total = 0;
-      for (PosTaggedSentence testDatum : testData) {
-        List<String> prediction = trainedModel.tagWords(testDatum.getWords()).getPos();
-        List<String> actual = testDatum.getPos();
-        Preconditions.checkState(prediction.size() == actual.size());
-        for (int i = 0; i < prediction.size(); i++) {
-          total++;
+      MapReduceExecutor executor = MapReduceConfiguration.getMapReduceExecutor();
+      PosError error = executor.mapReduce(testData, new PosEvaluationMapper(trainedModel),
+          new PosEvaluationReducer());
 
-          if (prediction.get(i).equals(actual.get(i))) {
-            numCorrect++;
-          }
-        }
-
-        System.out.println(testDatum.getWords());
-        System.out.println(prediction);
-        System.out.println(actual);
-      }
-      
-      double accuracy = ((double) numCorrect) / total;
-      System.out.println("PER-WORD ACCURACY: " + accuracy + " (" + numCorrect + " / " + total + ")");
+      System.out.println("TAG ACCURACY: " + error.getTagAccuracy() + " (" + error.getNumTagsCorrect() + " / " + error.getNumTags() + ")");
+      System.out.println("SENTENCE ACCURACY: " + error.getSentenceAccuracy() + " (" + error.getNumSentencesCorrect() + " / " + error.getNumSentences() + ")");
     } else {
       PosTaggedSentence tags = trainedModel.tagWords(options.nonOptionArguments());
       System.out.println(tags.getWords());
@@ -64,5 +53,95 @@ public class TestPosCrf extends AbstractCli {
 
   public static void main(String[] args) {
     new TestPosCrf().run(args);
+  }
+  
+  private static class PosError {
+    private int numTagsCorrect;
+    private int numTags;
+    
+    private int numSentencesCorrect;
+    private int numSentences;
+
+    public PosError(int numTagsCorrect, int numTags, int numSentencesCorrect, int numSentences) {
+      this.numTagsCorrect = numTagsCorrect;
+      this.numTags = numTags;
+      this.numSentencesCorrect = numSentencesCorrect;
+      this.numSentences = numSentences;
+    }
+    
+    public static PosError zero() {
+      return new PosError(0, 0, 0, 0);
+    }
+    
+    public int getNumTagsCorrect() {
+      return numTagsCorrect;
+    }
+
+    public int getNumTags() {
+      return numTags;
+    }
+    
+    public double getTagAccuracy() {
+      return ((double) numTagsCorrect) / numTags;
+    }
+
+    public int getNumSentencesCorrect() {
+      return numSentencesCorrect;
+    }
+
+    public int getNumSentences() {
+      return numSentences;
+    }
+    
+    public double getSentenceAccuracy() {
+      return ((double) numSentencesCorrect) / numSentences;
+    }
+
+    public void increment(PosError other) {
+      this.numTagsCorrect += other.numTagsCorrect;
+      this.numTags += other.numTags;
+      this.numSentencesCorrect += other.numSentencesCorrect;
+      this.numSentences += other.numSentences;
+    }
+  }
+  
+  private static class PosEvaluationMapper extends Mapper<PosTaggedSentence, PosError> {
+    
+    private final PosTagger tagger;
+    
+    public PosEvaluationMapper(PosTagger tagger) {
+      this.tagger = Preconditions.checkNotNull(tagger);
+    }
+
+    @Override
+    public PosError map(PosTaggedSentence item) {
+      List<String> prediction = tagger.tagWords(item.getWords()).getPos();
+      List<String> actual = item.getPos();
+      Preconditions.checkState(prediction.size() == actual.size());
+      int numTagsCorrect = 0;
+      for (int i = 0; i < prediction.size(); i++) {
+        if (prediction.get(i).equals(actual.get(i))) {
+          numTagsCorrect++;
+        }
+      }
+
+      System.out.println(item.getWords() + "\n" + prediction + "\n" + actual);
+      int numSentencesCorrect = (numTagsCorrect == actual.size()) ? 1 : 0;
+      return new PosError(numTagsCorrect, actual.size(), numSentencesCorrect, 1);
+    }
+  }
+  
+  private static class PosEvaluationReducer extends SimpleReducer<PosError> {
+
+    @Override
+    public PosError getInitialValue() {
+      return PosError.zero();
+    }
+
+    @Override
+    public PosError reduce(PosError item, PosError accumulated) {
+      accumulated.increment(item);
+      return accumulated;
+    }
   }
 }

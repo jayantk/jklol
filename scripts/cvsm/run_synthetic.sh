b6f846c7d436b7ea4be7890ff0cc81458edc589a
Jayant Krishnamurthy <jayantk@cs.cmu.edu>
2013-04-12 23:32:37 -0400

    added ability to initialize vectors

--- scripts/cvsm/run_synthetic.sh
+++ scripts/cvsm/run_synthetic.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-DATA=data/cvsm/synthetic/sizes/tensor.txt
+DATA=data/cvsm/synthetic/logic/tensor.txt
+INITIAL_VECTORS=data/cvsm/synthetic/logic_vectors.txt
 
-./scripts/run.sh com.jayantkrish.jklol.cvsm.TrainCvsm --training $DATA --output out.ser --iterations 10000 --batchSize 1 --l2Regularization 0.001 --brief
-./scripts/run.sh com.jayantkrish.jklol.cvsm.TestCvsm --testFilename $DATA --model out.ser 
+./scripts/run.sh com.jayantkrish.jklol.cvsm.TrainCvsm --training $DATA --output out.ser --iterations 10000 --batchSize 1 --l2Regularization 0.001 --brief --initialVectors=$INITIAL_VECTORS
+./scripts/run.sh com.jayantkrish.jklol.cvsm.TestCvsm  --model out.ser  --testFilename $DATA

01b4104f0db60750e005566d3c802ea90a4ed494
Jayant Krishnamurthy <jayantk@gs9670.sp.cs.cmu.edu>
2013-04-15 08:26:01 -0400

    rnn data set

--- scripts/cvsm/run_synthetic.sh
+++ scripts/cvsm/run_synthetic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DATA=data/cvsm/synthetic/sizes/tensor.txt
+DATA=data/cvsm/synthetic/logic/rnn.txt
 
 ./scripts/run.sh com.jayantkrish.jklol.cvsm.TrainCvsm --training $DATA --output out.ser --iterations 10000 --batchSize 1 --l2Regularization 0.001 --brief
 ./scripts/run.sh com.jayantkrish.jklol.cvsm.TestCvsm --testFilename $DATA --model out.ser 

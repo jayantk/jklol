68f108612aac9e91a2df26dffb1262685d1eabe6
Jayant Krishnamurthy <jayantk@cs.cmu.edu>
2013-04-18 22:43:26 -0400

    minor documentation tweak

--- src/com/jayantkrish/jklol/boost/package-info.java
+++ src/com/jayantkrish/jklol/boost/package-info.java
@@ -1,6 +1,6 @@
 /**
  * Implementation of boosting for training ensembles of 
- * graphical models. This package generically implements 
+ * structured models. This package generically implements 
  * a generic functional gradient ascent version of boosting 
  * that can apply to both simple models (e.g., classifiers) 
  * as well as structured models (e.g., graphical models, 

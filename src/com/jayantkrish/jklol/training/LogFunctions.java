94a9e13f2c5a6a7d1ac0cf31b30c7355ddcc53a1
Jayant Krishnamurthy <me@jayantkrish.com>
2013-06-11 20:31:10 -0400

    making logging more centralized

---
+++ src/com/jayantkrish/jklol/training/LogFunctions.java
@@ -0,0 +1,17 @@
+package com.jayantkrish.jklol.training;
+
+public class LogFunctions {
+
+    private static LogFunction log = null;
+
+    public static void setLogFunction(LogFunction newLog) {
+	log = newLog;
+    }
+
+    public static LogFunction getLogFunction() {
+	if (log == null) {
+	    log = new DefaultLogFunction();
+	}
+	return log;
+    }
+}

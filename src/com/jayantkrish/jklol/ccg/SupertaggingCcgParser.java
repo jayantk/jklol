e5f4d1bf006f6534b704fdb952ee263c924ea008
Jayant Krishnamurthy <jayantk@gs9670.sp.cs.cmu.edu>
2014-02-06 14:17:13 -0500

    javadocing

--- src/com/jayantkrish/jklol/ccg/SupertaggingCcgParser.java
+++ src/com/jayantkrish/jklol/ccg/SupertaggingCcgParser.java
@@ -12,6 +12,16 @@ import com.jayantkrish.jklol.ccg.supertag.Supertagger;
 import com.jayantkrish.jklol.ccg.supertag.WordAndPos;
 import com.jayantkrish.jklol.training.NullLogFunction;
 
+/**
+ * A combined CCG parser and supertagger. This parser accepts 
+ * POS-tagged sentences, supertags them, then parses them.
+ * The parser can be configured to use a backoff strategy that
+ * adjusts the supertagger's multitag threshold (which affects
+ * the number of supertags assigned to each token) when CCG
+ * parsing fails.
+ *
+ * @author jayantk
+ */
 public class SupertaggingCcgParser {
   private final CcgParser parser;
   private final CcgInference inference;
@@ -30,6 +40,17 @@ public class SupertaggingCcgParser {
     this.multitagThresholds = Arrays.copyOf(multitagThresholds, multitagThresholds.length);
   }
 
+  /**
+   * CCG parses {@code sentence}. Any supertags in {@code sentence}
+   * are ignored; instead, this method uses the supertags from 
+   * the parser's internal supertagger. {@code inputFilter} is an
+   * additional cost to use during parsing. {@code inputFilter} may be
+   * {@code null}, in which case no additional cost is used. 
+   *  
+   * @param sentence
+   * @param inputFilter
+   * @return
+   */
   public CcgParseResult parse(SupertaggedSentence sentence, ChartCost inputFilter) {
     SupertaggedSentence supertaggedSentence = null;
     if (supertagger != null) {
@@ -60,6 +81,13 @@ public class SupertaggingCcgParser {
     }
   }
 
+  /**
+   * Same as {@link #parse(SupertaggedSentence, ChartCost)},
+   * without a {@code ChartCost}.
+   *  
+   * @param sentence
+   * @return
+   */
   public CcgParseResult parse(SupertaggedSentence sentence) {
     return parse(sentence, null);
   }

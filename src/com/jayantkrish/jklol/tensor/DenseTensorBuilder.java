a4dc8d5f275554b2fd95d7177fc2ec1303ff044c
Jayant Krishnamurthy <jayantk@cs.cmu.edu>
2014-12-27 17:48:55 +0100

    trying new thing

--- src/com/jayantkrish/jklol/tensor/DenseTensorBuilder.java
+++ src/com/jayantkrish/jklol/tensor/DenseTensorBuilder.java
@@ -67,12 +67,11 @@ public class DenseTensorBuilder extends DenseTensorBase implements TensorBuilder
 
   @Override
   public void increment(double amount) {
-    info.yeppp.Core.Add_IV64fS64f_IV64f(values, 0, amount, values.length);
-    /*
+    // Not optimized
+    // info.yeppp.Core.Add_IV64fS64f_IV64f(values, 0, amount, values.length);
     for (int i = 0; i < values.length; i++) {
       values[i] += amount;
     }
-    */
   }
 
   @Override
@@ -300,11 +299,14 @@ public class DenseTensorBuilder extends DenseTensorBase implements TensorBuilder
     if (other instanceof DenseTensorBase) {
       DenseTensorBase otherTensor = (DenseTensorBase) other;
       Preconditions.checkArgument(otherTensor.values.length == values.length);
-      // info.yeppp.Core.Multiply_IV64fV64f_IV64f(values, 0, otherTensor.values, 0, values.length);
 
+      info.yeppp.Core.Multiply_V64fV64f_V64f(values, 0, otherTensor.values, 0, values, 0, values.length);
+
+      /*
       for (int i = 0; i < values.length; i++) {
         values[i] *= otherTensor.values[i];
       }
+      */
     } else {
       Iterator<KeyValue> keyValueIter = keyValueIterator();
       while (keyValueIter.hasNext()) {
@@ -316,12 +318,11 @@ public class DenseTensorBuilder extends DenseTensorBase implements TensorBuilder
 
   @Override
   public void multiply(double amount) {
-    info.yeppp.Core.Multiply_V64fS64f_V64f(values, 0, amount, values, 0, values.length);
-    /*
+    // Not optimized.
+    // info.yeppp.Core.Multiply_V64fS64f_V64f(values, 0, amount, values, 0, values.length);
     for (int i = 0; i < values.length; i++) {
       values[i] *= amount;
     }
-    */
   }
 
   @Override

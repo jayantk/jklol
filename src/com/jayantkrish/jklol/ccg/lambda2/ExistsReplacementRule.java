747998181e9f9691a380719912d1124e31c4de45
Jayant Krishnamurthy <me@jayantkrish.com>
2015-04-18 12:29:43 -0400

    working on existential quantifier thingy

---
+++ src/com/jayantkrish/jklol/ccg/lambda2/ExistsReplacementRule.java
@@ -0,0 +1,10 @@
+package com.jayantkrish.jklol.ccg.lambda2;
+
+public class ExistsReplacementRule implements ExpressionReplacementRule {
+
+  @Override
+  public Expression2 getReplacement(Expression2 input, int index) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+}

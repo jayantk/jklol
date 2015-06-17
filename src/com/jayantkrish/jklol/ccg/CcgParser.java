8a61729a6fbf169bf4510c0c77402a2cc0ec706f
Jayant Krishnamurthy <jayantk@jayantk.local>
2015-06-17 16:47:29 -0700

    deleting unused code

--- src/com/jayantkrish/jklol/ccg/CcgParser.java
+++ src/com/jayantkrish/jklol/ccg/CcgParser.java
@@ -1195,13 +1195,8 @@ public class CcgParser implements Serializable {
     parseCommon(chart, input, beamFilter, log, maxParseTimeMillis, numThreads);
 
     if (chart.isFinishedParsing()) {
-      if (allowWordSkipping) {
         return addSentenceToParse(chart.decodeBestParseForSubspan(
             0, chart.size() - 1, this), chart);
-      } else {
-        return addSentenceToParse(chart.decodeBestParseForSpan(
-            0, chart.size() - 1, this), chart);
-      }
     } else {
       System.out.println("CCG Parser Timeout");
       return null;

e44ae2579038aa476b1025134519f1cae4cd9914
Jayant Krishnamurthy <me@jayantkrish.com>
2015-06-26 15:02:43 -0400

    lexicon induction changes

--- experiments/geoquery/scripts/induce_lexicon.sh
+++ experiments/geoquery/scripts/induce_lexicon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-TRAINING_DATA=experiments/geoquery/data/fold0.ccg
+TRAINING_DATA=experiments/geoquery/data/all_folds.ccg
 # TEST_DATA=experiments/geoquery/data/all_folds.ccg
 
 RULES=experiments/geoquery/grammar/rules.txt
@@ -9,7 +9,7 @@ LEXICON=experiments/geoquery/grammar/generated_lexicon.txt
 ALIGNMENT_MODEL=alignment_out.ser
 MODEL=out.ser
 
-./scripts/run.sh com.jayantkrish.jklol.ccg.cli.AlignmentLexiconInduction --trainingData $TRAINING_DATA --lexiconOutput $LEXICON --modelOutput $ALIGNMENT_MODEL --maxThreads 1 --emIterations 30 --smoothing 0.01 --useCfg --nGramLength 1
+./scripts/run.sh com.jayantkrish.jklol.ccg.cli.AlignmentLexiconInduction --trainingData $TRAINING_DATA --lexiconOutput $LEXICON --modelOutput $ALIGNMENT_MODEL --maxThreads 1 --emIterations 10 --smoothing 0.01 --useCfg --nGramLength 1 
 
 # ./scripts/run.sh com.jayantkrish.jklol.ccg.cli.TrainSemanticParser --trainingData $TRAINING_DATA --lexicon $LEXICON --rules $RULES --skipWords  --batchSize 1 --iterations 20 --output $MODEL --logInterval 10 --beamSize 500 --returnAveragedParameters --alignmentModel $ALIGNMENT_MODEL
 

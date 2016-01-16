a3395faa18e06252a12a930931ff6dae86b6013f
Jayant Krishnamurthy <me@jayantkrish.com>
2016-01-16 12:33:27 -0800

    refactoring shift reduce inference

--- src/com/jayantkrish/jklol/ccg/CcgShiftReduceInference.java
+++ src/com/jayantkrish/jklol/ccg/CcgShiftReduceInference.java
@@ -2,6 +2,7 @@ package com.jayantkrish.jklol.ccg;
 
 import java.util.List;
 
+import com.jayantkrish.jklol.ccg.chart.CcgChart;
 import com.jayantkrish.jklol.ccg.chart.CcgLeftToRightChart;
 import com.jayantkrish.jklol.ccg.chart.ChartCost;
 import com.jayantkrish.jklol.ccg.chart.ChartEntry;
@@ -41,90 +42,34 @@ public class CcgShiftReduceInference implements CcgInference {
     CcgLeftToRightChart chart = new CcgLeftToRightChart(sentence, Integer.MAX_VALUE);
     parser.initializeChart(chart, sentence, chartFilter);
     parser.initializeChartTerminals(chart, sentence);
-    
-    int inputLength = sentence.getWords().size();
 
     // Working heap for queuing parses to process next.
-    double[] heapValues = new double[beamSize + 1];
-    Stack[] heapKeys = new Stack[beamSize + 1];
-    int heapSize = 0;
-    heapSize = queue(Stack.empty(), heapKeys, heapValues, heapSize);
+    Heap heap = new Heap(beamSize);
+    heap.queue(Stack.empty());
 
     // Array of elements in the current beam.
     Stack[] currentBeam = new Stack[beamSize + 1];
     int currentBeamSize = 0;
 
-    while (heapSize > 0) {
+    while (heap.size() > 0) {
       // Copy the heap to the current beam.
-      for (int i = 0; i < heapSize; i++) {
-        currentBeam[i] = heapKeys[i];
+      Stack[] keys = heap.getKeys();
+      for (int i = 0; i < heap.size(); i++) {
+        currentBeam[i] = keys[i];
       }
       
       // System.out.println();
       // System.out.println("LOOP: " + heapSize);
 
-      // Empty out the heap.
-      currentBeamSize = heapSize;
-      heapSize = 0;
+      // Empty the heap.
+      currentBeamSize = heap.size();
+      heap.clear();
       
       for (int i = 0; i < currentBeamSize; i++) {
         Stack stack = currentBeam[i];
-        int curToken = stack.spanEnd + 1;
         // System.out.println("Processing " + stack);
-
-        // Perform SHIFT actions.
-        // The possible shift actions are the chart entries at
-        // the span (curToken, curToken) to the span (curToken, inputLength - 1).
-        // There is more than one possible span because lexicons may
-        // contain entries spanning multiple tokens.
-        // Also note that CcgChart uses an inclusive end index.
-        for (int spanEnd = curToken; spanEnd < inputLength; spanEnd++) {
-          ChartEntry[] entries = chart.getChartEntriesForSpan(curToken, spanEnd);
-          double[] probs = chart.getChartEntryProbsForSpan(curToken, spanEnd);
-          int numEntries = chart.getNumChartEntriesForSpan(curToken, spanEnd);
-
-          for (int j = 0; j < numEntries; j++) {
-            // Some nonterminal entries may have been added to chart
-            // by other parses in the beam that have processed more
-            // tokens than this parse.
-            if (entries[j].isTerminal()) {
-              // System.out.println("SHIFT: " + curToken + "," + spanEnd);
-              
-              // Queue the shift action by adding it to the heap.
-              Stack nextStack = stack.push(curToken, spanEnd, j, entries[j], probs[j]);
-              heapSize = queue(nextStack, heapKeys, heapValues, heapSize);
-            }
-          }
-        }
-
-        // Perform REDUCE actions.
-        if (stack.size > 1) {
-          Stack prev = stack.previous;
-
-          ChartEntry[] prevEntryArray = chart.getChartEntriesForSpan(prev.spanStart, prev.spanEnd);
-          double[] prevEntryProbArray = chart.getChartEntryProbsForSpan(prev.spanStart, prev.spanEnd);
-          IntMultimap prevTypes = IntMultimap.createFromUnsortedArrays(
-              new int[] {prev.entry.getHeadedSyntax()}, new int[] {prev.chartEntryIndex}, 0);
-          
-          ChartEntry[] stackEntryArray = chart.getChartEntriesForSpan(stack.spanStart, stack.spanEnd);
-          double[] stackEntryProbArray = chart.getChartEntryProbsForSpan(stack.spanStart, stack.spanEnd);
-          IntMultimap stackTypes = IntMultimap.createFromUnsortedArrays(
-              new int[] {stack.entry.getHeadedSyntax()}, new int[] {stack.chartEntryIndex}, 0);
-          
-          int startNumEntries = chart.getNumChartEntriesForSpan(prev.spanStart, stack.spanEnd);
-          parser.applySearchMoves(chart, prev.spanStart, prev.spanEnd, stack.spanStart, stack.spanEnd,
-              prevEntryArray, prevEntryProbArray, prevTypes, stackEntryArray, stackEntryProbArray, stackTypes,
-              log);
-          int endNumEntries = chart.getNumChartEntriesForSpan(prev.spanStart, stack.spanEnd);
-
-          ChartEntry[] chartEntries = chart.getChartEntriesForSpan(prev.spanStart, stack.spanEnd);
-          double[] chartProbs = chart.getChartEntryProbsForSpan(prev.spanStart, stack.spanEnd);
-          for (int j = startNumEntries; j < endNumEntries; j++) {
-            // System.out.println("REDUCE: " + stack + " --> " + parser.getSyntaxVarType().getValue(chartEntries[j].getHeadedSyntax()));
-            Stack toQueue = prev.previous.push(prev.spanStart, stack.spanEnd, j, chartEntries[j], chartProbs[j]);
-            heapSize = queue(toQueue, heapKeys, heapValues, heapSize);
-          }
-        }
+        shift(stack, chart, heap);
+        reduce(stack, chart, heap, parser, log);
       }
     }
 
@@ -134,14 +79,65 @@ public class CcgShiftReduceInference implements CcgInference {
     return chart.decodeBestParsesForSpan(0, chart.size() - 1, beamSize, parser);
   }
   
-  private final int queue(Stack toQueue, Stack[] heapKeys, double[] heapValues, int heapSize) {
-    HeapUtils.offer(heapKeys, heapValues, heapSize, toQueue, toQueue.totalProb);
-    heapSize++;
-    if (heapSize > beamSize) {
-      HeapUtils.removeMin(heapKeys, heapValues, heapSize);
-      heapSize--;
+  public static final void shift(Stack stack, CcgChart chart, Heap heap) {
+    // Perform SHIFT actions.
+    // The possible shift actions are the chart entries at
+    // the span (curToken, curToken) to the span (curToken, inputLength - 1).
+    // There is more than one possible span because lexicons may
+    // contain entries spanning multiple tokens.
+    // Also note that CcgChart uses an inclusive end index.
+    int curToken = stack.spanEnd + 1;
+    int inputLength = chart.getWords().size();
+    for (int spanEnd = curToken; spanEnd < inputLength; spanEnd++) {
+      ChartEntry[] entries = chart.getChartEntriesForSpan(curToken, spanEnd);
+      double[] probs = chart.getChartEntryProbsForSpan(curToken, spanEnd);
+      int numEntries = chart.getNumChartEntriesForSpan(curToken, spanEnd);
+
+      for (int j = 0; j < numEntries; j++) {
+        // Some nonterminal entries may have been added to chart
+        // by other parses in the beam that have processed more
+        // tokens than this parse.
+        if (entries[j].isTerminal()) {
+          // System.out.println("SHIFT: " + curToken + "," + spanEnd);
+
+          // Queue the shift action by adding it to the heap.
+          Stack nextStack = stack.push(curToken, spanEnd, j, entries[j], probs[j]);
+          heap.queue(nextStack);
+        }
+      }
     }
-    return heapSize;
+  }
+  
+  public static final void reduce(Stack stack, CcgChart chart, Heap heap, CcgParser parser,
+      LogFunction log) {
+    // Perform REDUCE actions.
+    if (stack.size > 1) {
+      Stack prev = stack.previous;
+
+      ChartEntry[] prevEntryArray = chart.getChartEntriesForSpan(prev.spanStart, prev.spanEnd);
+      double[] prevEntryProbArray = chart.getChartEntryProbsForSpan(prev.spanStart, prev.spanEnd);
+      IntMultimap prevTypes = IntMultimap.createFromUnsortedArrays(
+          new int[] {prev.entry.getHeadedSyntax()}, new int[] {prev.chartEntryIndex}, 0);
+
+      ChartEntry[] stackEntryArray = chart.getChartEntriesForSpan(stack.spanStart, stack.spanEnd);
+      double[] stackEntryProbArray = chart.getChartEntryProbsForSpan(stack.spanStart, stack.spanEnd);
+      IntMultimap stackTypes = IntMultimap.createFromUnsortedArrays(
+          new int[] {stack.entry.getHeadedSyntax()}, new int[] {stack.chartEntryIndex}, 0);
+
+      int startNumEntries = chart.getNumChartEntriesForSpan(prev.spanStart, stack.spanEnd);
+      parser.applySearchMoves(chart, prev.spanStart, prev.spanEnd, stack.spanStart, stack.spanEnd,
+          prevEntryArray, prevEntryProbArray, prevTypes, stackEntryArray, stackEntryProbArray, stackTypes,
+          log);
+      int endNumEntries = chart.getNumChartEntriesForSpan(prev.spanStart, stack.spanEnd);
+
+      ChartEntry[] chartEntries = chart.getChartEntriesForSpan(prev.spanStart, stack.spanEnd);
+      double[] chartProbs = chart.getChartEntryProbsForSpan(prev.spanStart, stack.spanEnd);
+      for (int j = startNumEntries; j < endNumEntries; j++) {
+        // System.out.println("REDUCE: " + stack + " --> " + parser.getSyntaxVarType().getValue(chartEntries[j].getHeadedSyntax()));
+        Stack toQueue = prev.previous.push(prev.spanStart, stack.spanEnd, j, chartEntries[j], chartProbs[j]);
+        heap.queue(toQueue);
+      }
+    }
   }
   
   private static class Stack {
@@ -187,6 +183,43 @@ public class CcgShiftReduceInference implements CcgInference {
     public String toString() {
       String prevString = previous == null ? "" : previous.toString();
       return spanStart + "," + spanEnd + " : " + prevString;
+    }
+  }
+  
+  private static class Heap {
+    private double[] values;
+    private Stack[] keys;
+    private int size;
+    private int maxElements;
+    
+    public Heap(int maxElements) {
+      values = new double[maxElements + 1];
+      keys = new Stack[maxElements + 1];
+      size = 0;
+      this.maxElements = maxElements;
+    }
+
+    public final int queue(Stack toQueue) {
+      HeapUtils.offer(keys, values, size, toQueue, toQueue.totalProb);
+      size++;
+
+      if (size > maxElements) {
+        HeapUtils.removeMin(keys, values, size);
+        size--;
+      }
+      return size;
+    }
+    
+    public int size() {
+      return size;
+    }
+    
+    public Stack[] getKeys() {
+      return keys;
+    }
+    
+    public void clear() {
+      this.size = 0;
     }
   }
 }

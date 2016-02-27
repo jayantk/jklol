2ec9d642641d7736c06a8be769cd59da9c49c434
Jayant Krishnamurthy <jayantk@allenai.org>
2016-02-27 12:54:01 -0800

    test cases for inference

---
+++ test/com/jayantkrish/jklol/ccg/gi/GroundedParserInterleavedInferenceTest.java
@@ -0,0 +1,8 @@
+package com.jayantkrish.jklol.ccg.gi;
+
+public class GroundedParserInterleavedInferenceTest extends GroundedParserTest {
+
+  public GroundedParserInterleavedInferenceTest() {
+    super(new GroundedParserInterleavedInference(10, -1));
+  }
+}
---
+++ test/com/jayantkrish/jklol/ccg/gi/GroundedParserPipelinedInferenceTest.java
@@ -0,0 +1,10 @@
+package com.jayantkrish.jklol.ccg.gi;
+
+import com.jayantkrish.jklol.ccg.CcgCkyInference;
+
+public class GroundedParserPipelinedInferenceTest extends GroundedParserTest {
+  
+  public GroundedParserPipelinedInferenceTest() {
+    super(new GroundedParserPipelinedInference(CcgCkyInference.getDefault(100), 10, 100));
+  }
+}

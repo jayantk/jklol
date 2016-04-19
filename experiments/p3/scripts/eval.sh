7c895bfc8da3f558f942b9734d96e62342952534
Jayant Krishnamurthy <jayantk@allenai.org>
2016-04-19 20:31:47 +0000

    messing with scripts

--- experiments/p3/scripts/eval.sh
+++ experiments/p3/scripts/eval.sh
@@ -2,5 +2,7 @@
 
 source experiments/p3/scripts/config.sh
 
+echo "Results for: $EXPERIMENT_NAME"
+
 grep 'Recall:' $EXPERIMENT_DIR/**/train_err.txt | grep -o '([^)]*)' | sed 's/[(\/)]//g' | awk '{SUM += $1; TOT += $2} END {print "Training recall: " (SUM / TOT) " (" SUM " / " TOT ")"}'
 grep 'Recall:' $EXPERIMENT_DIR/**/test_err.txt | grep -o '([^)]*)' | sed 's/[(\/)]//g' | awk '{SUM += $1; TOT += $2} END {print "Test recall: " (SUM / TOT) " (" SUM " / " TOT ")"}'

9d18daf3e2ffd0bf2a41a9695453ffeb7041f569
Jayant Krishnamurthy <jayantk@allenai.org>
2016-06-10 14:53:11 -0700

    updating scripts and geoquery data for example

--- experiments/geoquery/scripts/induce_lexicon_cv.sh
+++ experiments/geoquery/scripts/induce_lexicon_cv.sh
@@ -5,9 +5,13 @@ OUT_DIR=experiments/geoquery/output/
 
 ENTITY_LEXICON=$OUT_DIR/entity_lexicon.txt
 
+FOLD_NUM=0
+
 mkdir -p $OUT_DIR
 
-./experiments/geoquery/scripts/generate_lexicon.py > $ENTITY_LEXICON
+./experiments/geoquery/scripts/convert_np_list.py experiments/geoquery/grammar/np-list.lex > $ENTITY_LEXICON
 
-./scripts/run.sh com.jayantkrish.jklol.experiments.geoquery.LexiconInductionCrossValidation --trainingDataFolds $DATA_DIR --outputDir $OUT_DIR --emIterations 100 --smoothing 0.1 --parserIterations 50 --beamSize 100 --l2Regularization 0.0 --additionalLexicon $ENTITY_LEXICON --unknownWordThreshold 0
+./scripts/run.sh com.jayantkrish.jklol.experiments.geoquery.GeoqueryInduceLexicon --trainingDataFolds $DATA_DIR --outputDir $OUT_DIR --emIterations 10 --smoothing 0.1 --parserIterations 10 --beamSize 100 --l2Regularization 0.0 --additionalLexicon $ENTITY_LEXICON --foldName fold$FOLD_NUM.ccg --unknownWordThreshold 0 --maxBatchesPerThread 1 --maxThreads 4 > $OUT_DIR/log_fold$FOLD_NUM.txt
 
+cat $OUT_DIR/training_error.fold$FOLD_NUM.ccg.json | jq '.correct' | awk '{SUM += $1; TOTAL += 1}; END {print "Training Accuracy"; print SUM / TOTAL}'
+cat $OUT_DIR/test_error.fold$FOLD_NUM.ccg.json | jq '.correct' | awk '{SUM += $1; TOTAL += 1}; END {print "Test Accuracy"; print SUM / TOTAL}'
